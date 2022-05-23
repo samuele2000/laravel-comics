992a08949d2690887f19a069f05395855b64afe7
samuele <spadaro.samuele@gmail.com>
2022-05-23 16:30:31 +0200

    header e jumbo fatti

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,39 @@
+<nav>
+    <div>
+        <img src="{{asset ('images/dc-logo.png')}}" alt="logo">
+    </div>
+    <div>
+        <ul>
+            <li>
+                <a href="/characters">CHARACTERS</a>
+            </li>
+            <li>
+                <a class="select" href="/characters">COMICS</a>
+            </li>
+            <li>
+                <a href="/characters">MOVIES</a>
+            </li>
+            <li>
+                <a href="/characters">TV</a>
+            </li>
+            <li>
+                <a href="/characters">GAMES</a>
+            </li>
+            <li>
+                <a href="/characters">COLLECTIBLES</a>
+            </li>
+            <li>
+                <a href="/characters">VIDEOS</a>
+            </li>
+            <li>
+                <a href="/characters">FANS</a>
+            </li>
+            <li>
+                <a href="/characters">NEWS</a>
+            </li>
+            <li>
+                <a href="/characters">SHOP</a>
+            </li>
+        </ul>
+    </div>
+</nav>
